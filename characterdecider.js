65223f1f6eabab86df4fd2f9e272a2261c278bbd
strangewillstrange <52141113+strangewillstrange@users.noreply.github.com>
2019-06-23 16:18:45 -0700

    Character_decider
    This is my test of "if" and "switch" statements in JS.

---
+++ characterdecider.js
@@ -0,0 +1,41 @@
+var name = prompt("what is your character Name?");
+
+if (name != null) {
+	alert("hello, " + name);
+}
+
+var role = prompt("Will you fight or cast?");
+
+if (role == "fight") {
+	fighter = prompt("What gives you strength? \nskill, \ngod, \nrage");
+	switch (fighter) {
+		case "skill":
+			alert(name + ", You should be a fighter.");
+			break;
+		case "god":
+			alert(name + ", You should be a Paladin.");
+			break;
+		case "rage":
+			alert(name + ", you should be a Barbarian.");
+			break;
+		default:
+			alert("try again");
+			break;
+		} 
+	} else {
+		var caster = prompt("Where does your magic come from? \nspellbook, \ngod, \nmusic");
+		switch (caster) {
+			case "spellbook":
+				alert(name + ", You should be a Wizard.");
+				break;
+			case "god":
+				alert(name + ", You should be a Cleric.");
+				break;
+			case "music":
+				alert(name + ", you should be a Bard.");
+				break;
+			default:
+				alert("try again");
+				break;
+		}
+	}
